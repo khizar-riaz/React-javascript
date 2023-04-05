97675f3ed86601a1b58defddbe8024bc9bba2043
khizar7422 <khizarriaz012@gmail.com>
2023-04-05 15:52:07 +0200

    Header update in develop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './MyComponents/Header'
+import Header from './MyComponents/Header'  //rfc default function import without brackets
+import { Todos } from "./MyComponents/Todos"; //rafc  const function import with brackets and spece between
+import { Footer  } from "./MyComponents/Footer";
 function App() {
   return (
     <>
-      <Header/>
+      <Header title="TO DO LIST" searchBar={true}/>
+      <Todos/>
+      <Footer/>
+      
     </>
   );
 }
