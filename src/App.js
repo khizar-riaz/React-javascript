0ffc66159fd74b13c7ba7de00def70a3168d50c6
khizar7422 <khizarriaz012@gmail.com>
2023-04-05 14:57:54 +0200

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './MyComponents/Header'
 function App() {
   return (
     <>
-  <ul class="nav">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
+      <Header/>
     </>
   );
 }
