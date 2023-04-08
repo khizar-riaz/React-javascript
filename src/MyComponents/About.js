73e19b50d4991c4c4a8e5fc52141fca9ef5675b4
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-08 15:19:50 +0200

    react router part one

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      this is an About component
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+        repellendus ipsa sequi quos fuga explicabo consequuntur quisquam error
+        veniam laborum, iste numquam dolorem quas excepturi laboriosam
+        doloremque ad unde quod?
+      </p>
+    </div>
+  );
+};
